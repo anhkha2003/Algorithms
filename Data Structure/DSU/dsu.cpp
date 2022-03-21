fde973939e34bf0d30655077e24dfc2d41e5e486
anhkha2003 <lephuocanhkha2003@gmail.com>
2022-03-21 14:41:42 +0700

    Update DSU

--- Data Structure/DSU/dsu.cpp
+++ Data Structure/DSU/dsu.cpp
@@ -2,19 +2,18 @@ struct DSU {
     vector<int> root;
     int n;
     DSU(int n): n(n) {
+        // root[x] > 0: parent of x
+        // root[x] < 0: size of part include x
         root.assign(n + 1, -1);
     }
 
-    // root[x] > 0: parent of x
-    // root[x] < 0: size of part include x
-
+    // root[findRoot] < 0: size của TPLT
     int findRoot(int u) {
         while (root[u] >= 0) {
             u = root[u];
         }
         return u;
     }
-    // root[findRoot] < 0: size của TPLT
 
     bool merge(int x, int y) {
         x = findRoot(x);
