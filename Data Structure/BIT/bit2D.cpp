6b7827012d3e2b0733b8ba856fff09aabfe931ee
anhkha2003 <lephuocanhkha2003@gmail.com>
2023-02-14 23:40:43 -0600

    Update BIT2D

--- Data Structure/BIT/bit2D.cpp
+++ Data Structure/BIT/bit2D.cpp
@@ -1,27 +1,66 @@
-struct BIT {
-    int n, m;
-    vector<vector<int>> fen;
-    BIT(int n, int m): n(n), m(m) {
-        fen.resize(n + 1);
-        for (int i = 1; i <= n; i++) {
-            fen[i].resize(m + 1);
-        }
-    } 
+// Usage: Need to know all points to update beforehand using fakeUpdate.
+// BIT2D bit; bit.fakeUpdate(x, y); bit.init();
+// bit.add(x, y, val): add val to (x, y)
+// bit.getSum(x, y): get sum in rectangle (1, 1) -> (x, y).
+// Complexity: O(log^2) each query
 
-    void update(int x, int y, int val) {
+struct BIT {
+    int n;
+    vector<int> fen;
+    BIT(int n): n(n) {
+        fen.assign(n + 1, 0);
+    }
+
+    void add(int x, int val) {
         for (int i = x; i <= n; i += i & -i) {
-            for (int j = y; j <= m; j += j & -j) {
-                fen[i][j] = max(fen[i][j], val);
-            }
+            fen[i] += val;
         }
     }
 
-    int get(int x, int y) {
+    int getSum(int x) {
         int res = 0;
         for (int i = x; i; i -= i & -i) {
-            for (int j = y; j; j -= j & -j) {
-                res = max(res, fen[i][j]);
-            }
+            res += fen[i];
+        }
+
+        return res;
+    }
+};
+
+struct BIT2D {
+    int limx;
+    vector<vector<int>> ys;
+    vector<BIT> fen;
+
+    BIT2D(int limx): limx(limx) {
+        ys.resize(limx + 1);
+    }
+    void fakeUpdate(int x, int y) {
+        for (int i = x; i <= limx; i += i & -i) {
+            ys[i].push_back(y);
+        }
+    }
+    void init() {
+        for (auto &v: ys) {
+            sort(v.begin(), v.end());
+            fen.push_back(BIT(v.size()));
+        }
+    }
+
+    int index(int x, int y) {
+        return (int) (lower_bound(ys[x].begin(), ys[x].end(), y) - ys[x].begin() + 1);
+    }
+
+    void add(int x, int y, int val) {
+        for (int i = x; i <= limx; i += i & -i) {
+            fen[i].add(index(i, y), val);
+        }
+    }
+
+    int getSum(int x, int y) {
+        int res = 0;
+        for (int i = x; i; i -= i & -i) {
+            res += fen[i].getSum(index(i, y + 1) - 1);
         }
         return res;
     }
