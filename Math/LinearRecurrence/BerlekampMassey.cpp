7dd5152a736bda74229c0dda78d5a0cb2a7382e7
Phuoc Anh Kha Le <76896393+anhkha2003@users.noreply.github.com>
2024-09-29 22:30:50 -0500

    Add find k-th element in linear recurrence

--- Math/LinearRecurrence/BerlekampMassey.cpp
+++ Math/LinearRecurrence/BerlekampMassey.cpp
@@ -1,6 +1,8 @@
 // Find the recurrence (Berlekamp-Massey)
-// Time complexity: O(n^2)
+// For recurrence length n, need at least 2n elements of s 
+// Find coefficients of linear recurrence
 // Input: s[0..n-1], Output: c[0..d], such that s[i] = sum s[i-j]*c[j] for j = 1 to d
+// Time complexity: O(n^2)
 
 const long long MOD = 998244353;
 
@@ -21,7 +23,6 @@ vector<long long> berlekampMassey(vector<long long> s) {
             delta %= MOD;
         }
 
- 
         if (delta == 0) continue;
  
         if (f == -1) {
@@ -53,7 +54,49 @@ vector<long long> berlekampMassey(vector<long long> s) {
             }
             f = fp;
         }
-    }
-    
+    } 
     return c;
+}
+
+// Find k-th element of linear recurrence
+// find S[k], given first elements of S and c: S[i] = sum S[i-j]*c[j] for j = 1 to d
+// c uses 1-index
+// Time complexity: O(n^2 * log(k))
+long long linearRec(const vector<long long>& S, const vector<long long>& c, long long k) {
+    int n = int(c.size()) - 1;
+    assert(S.size() + 1 >= c.size());
+
+    auto combine = [&](vector<long long> a, vector<long long> b, bool e = false) {
+        // multiply a * b * x^e
+        vector<long long> res(int(a.size()) + int(b.size()));
+        for (int i = 0; i < int(a.size()); i++) {
+            for (int j = 0; j < int(b.size()); j++) {
+                res[i + j + e] += a[i] * b[j];
+                res[i + j + e] %= MOD;
+            }
+        }
+        for (int i = int(res.size())-1; i >= n; --i) {
+            for (int j = 1; j <= n; j++) {
+                res[i - j] += res[i] * c[j];
+                res[i - j] %= MOD;
+            }
+        }
+        res.resize(n);
+        return res;
+    };
+
+    vector<long long> pol(n);
+    if (n > 0) pol[0] = 1;
+
+    assert(k >= 0);
+    for (int i = 64 - 1 - (k == 0 ? 64 : __builtin_clzll(k)); i >= 0; i--) {
+        pol = combine(pol, pol, (k >> i) & 1);
+    }
+
+    long long res = 0;
+    for (int i = 0; i < n; i++) {
+        res += pol[i] * S[i];
+        res %= MOD;
+    }
+    return res;
 }
